bd7658ecd988b1bcaaa649d98ca412572a82add4
Nicolas Guerrero <Guerreronicolas1872@gmail.com>
2021-10-11 20:21:42 -0700

    Add font colors and buttons
    Adds the specified colors to the font. Also includes
    vertical alignment on baseline, although it's a bit
    too bottom heavy now. App.css will likely need a clean
    up during the review phase.

--- react-frontend/src/components/homeMasthead.js
+++ react-frontend/src/components/homeMasthead.js
@@ -10,18 +10,22 @@ export default class HomeMasthead extends Component {
     render(){
         // Before render
         return(
-            <div className="w-100">
-                <div className="row my-auto">
-                <div className="col-7">
-                    <div className="mx-auto">
-                        <h1>Food Finder</h1>
-                        <p>Start finding recipes now</p>
-                        <input type="text"/>
+            <div className="container-fluid">
+                <div className="d-flex align-items-center row">
+                    <div className="col-7">
+                        <div className="align-baseline">
+                            <h1 className="title">Food Finder</h1>
+                            <h2>Start finding recipes now</h2>
+                            <input type="text" className="my-3"/>
+                            <div className="row">
+                                <button className="buttonHome col-4 mx-2">Sign-In</button>
+                                <button className="buttonHome col-4 mx-2">Sign-Up</button>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className="col-5">
-                    <img className="img-fluid" src={head}/>
-                </div>
+                    <div className="col-5 px-0">
+                        <img className="img-fluid" src={head}/>
+                    </div>
                 </div>
             </div>
         )
