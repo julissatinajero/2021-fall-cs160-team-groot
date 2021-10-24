293af733a23496dc0a8d1298b894111f077a6034
Andrew Shinjo <shinjo1233@gmail.com>
2021-10-23 22:20:05 -0700

    combined frontend and backend on same port

---
+++ spring-backend/src/main/frontend/src/components/homeMasthead.js
@@ -0,0 +1,40 @@
+import React, {Component} from "react";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import '../App.css';
+import '../css/home.css';
+import head from '../resources/StockFood.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
+export default class HomeMasthead extends Component {
+
+    render(){
+        // Before render
+        return(
+            <div className="container-fluid">
+                <div className="d-flex align-items-center row">
+                    <div className="col-7">
+                        <div className="mb-5">
+                            <h1 className="title ff-font d-flex justify-content-center">Food Finder</h1>
+                            <h2 className="d-flex justify-content-center">Start finding recipes now</h2>
+                            <div className="d-flex justify-content-center my-3">
+                                <input type="text" className="searchBar py-1"/>
+                                <button className="buttonSearch col-1 py-1">
+                                    <Link to="/search"><FontAwesomeIcon icon={faSearch}/></Link>
+                                </button>
+                            </div>                            
+                            <div className="row d-flex justify-content-center">
+                                <button className="buttonHome col-4 mx-2">Sign-In</button>
+                                <button className="buttonHome col-4 mx-2">Sign-Up</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-5 px-0">
+                        <img className="img-fluid" src={head}/>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
---
+++ spring-backend/src/main/java/com/cs160groot/FoodFinder/Controller/TestController.java
@@ -0,0 +1,12 @@
+package com.cs160groot.FoodFinder.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+	@RequestMapping({ "/hello" })
+	public String hello() {
+		return "Hello, world!";
+	}
+}
