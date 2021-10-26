736377cc4069c14e664166fe84131b582ca5c2e0
Andrew Shinjo <shinjo1233@gmail.com>
2021-10-25 21:56:15 -0700

    Implemented authentication via json web tokens

--- spring-backend/src/main/java/com/cs160groot/FoodFinder/Entity/AppUser.java
+++ spring-backend/src/main/java/com/cs160groot/FoodFinder/Entity/AppUser.java
@@ -12,6 +12,8 @@ public class AppUser {
 	private String email;
 	private String username;
 	private String password;
+	private boolean active;
+	
 	
 	public AppUser() {}
 	
---
+++ spring-backend/src/main/java/com/cs160groot/FoodFinder/Security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.cs160groot.FoodFinder.Security;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.cs160groot.FoodFinder.Entity.AppUser;
+import com.cs160groot.FoodFinder.Repository.AppUserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	@Autowired
+	private AppUserRepository appUserRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<AppUser> appUser = appUserRepository.findByUsername(username);
+		appUser.orElseThrow(() -> new UsernameNotFoundException("Username not found."));
+		return appUser.map(UserDetailsImpl::new).get();
+	}
+}
