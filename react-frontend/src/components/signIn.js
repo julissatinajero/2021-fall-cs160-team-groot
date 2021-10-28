6ef3b263c89377d06a866615023dba3096156ace
Julissa Tinajero <julissatinajero99@gmail.com>
2021-10-28 13:41:21 -0700

    Fixes stylying issues with the sign in page (backgroud pic, element centering, etc)

--- react-frontend/src/components/signIn.js
+++ react-frontend/src/components/signIn.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { Form, Card } from 'react-bootstrap'
 import '../css/signIn.css';
 
@@ -7,25 +6,25 @@ export default class SignIn extends Component {
 
     render() {
         return (
-            <div className="center">
-                <Card style={{ width: '35rem' }}>
-                    <div className="padding">
-                        <h1 className="title-signIn ff-font center">Sign In</h1>
-                        <Form>
-                            <Form.Group controlId="formBasicText">
+            <div className="outer-container-signIn">
+                <Card style={{ width: "45%"}}>
+                    <Card.Body>
+                        <Card.Title><h1 class="text-center ff-font">Sign In</h1></Card.Title>
+                        <Form className="formStyling-signIn">
+                            <Form.Group controlId="signin-username">
                                 <Form.Label>Username</Form.Label>
                                 <Form.Control type="text" placeholder="Enter username" />
                             </Form.Group>
 
-                            <Form.Group controlId="formBasicPassword" style={{ paddingTop: '20px' }}>
+                            <Form.Group controlId="signin-password" style={{ paddingTop: '20px' }}>
                                 <Form.Label>Password</Form.Label>
                                 <Form.Control type="password" placeholder="Enter password" />
                             </Form.Group>
-                            <div className="center">
-                                <button className="signInButton" type="submit">Sign In</button>
+                            <div className="signIn-button-formatting">
+                                <button className="signIn-button-styling" type="submit">Sign In</button>
                             </div>
                         </Form>
-                    </div>
+                    </Card.Body>
                 </Card>
             </div>
         )
