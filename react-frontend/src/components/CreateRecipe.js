85bc27b9332e80c6182a8b63c87613b92f2e3038
Julissa Tinajero <julissatinajero99@gmail.com>
2021-10-28 18:34:38 -0700

    Added some of the basic components for Recipe Form

--- react-frontend/src/components/CreateRecipe.js
+++ react-frontend/src/components/CreateRecipe.js
@@ -1,22 +1,66 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import '../css/SignUp.css';
-import {Container, Button, Form, Card} from 'react-bootstrap';
+import { Container, Button, Form, Row, Col, Dropdown } from 'react-bootstrap';
 
 const CreateRecipePage = () => {
     return (
         <Container>
-            <Form>
-                <Form.Group className="mb-3" controlId="firstName">
-                    <Form.Label>First Name</Form.Label>
-                    <Form.Control 
-                        placeholder="Title" 
-                        name="title" 
+            <Form style={{ width: "80%", marginLeft: "10%", marginTop: "5%" }}>
+                <h1>Create a new recipe</h1>
+                <Form.Group className="mb-3" controlId="recipeTitle">
+                    <Form.Label>Title of Recipe</Form.Label>
+                    <Form.Control
+                        placeholder="Title"
+                        name="recipeTitle"
                     />
                 </Form.Group>
                 <div className="row">
                     <div className="Author + Date add"></div>
                     <div className="Prep time + diet"></div>
                 </div>
+                <Form.Group className="mb-3" controlId="recipeDescription">
+                    <Form.Label>Brief Description</Form.Label>
+                    <Form.Control
+                        placeholder="Description"
+                        name="recipeDescription"
+                    />
+                </Form.Group>
+                <Row>
+                    <div class="col-8">
+                        <Form.Group className="mb-3" controlId="instructions">
+                            <Form.Label>List the instructions</Form.Label>
+                            <Form.Control
+                                placeholder="Instruction"
+                                name="instructions"
+                            /> </Form.Group>
+                    </div>
+                    <div class="col-3">
+                        <Button>+</Button>
+                    </div>
+                </Row>
+                <Row>
+                    <div class="col-8">
+                        <Form.Group className="mb-3" controlId="ingredients">
+                            <Form.Label>List the ingredients</Form.Label>
+                            <Form.Control
+                                placeholder="Ingredient"
+                                name="ingredients"
+                            /> </Form.Group>
+                    </div>
+                    <div class="col-3">
+                        <Button>+</Button>
+                    </div>
+                </Row>
+                <Dropdown>
+                    <Dropdown.Toggle variant="success" id="dropdown-basic">Prep Time</Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="#/action-1">15 min</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">30 min</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">45 min</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">60 min</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
                 <div className="descriptions"></div>
                 <div className="ingredients"></div>
                 <div className="instructions"></div>
