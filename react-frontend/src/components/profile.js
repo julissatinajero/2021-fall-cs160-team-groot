fe2fbe412bdbf63675db1cca15cd790d59f65100
Andrew Shinjo <shinjo1233@gmail.com>
2021-11-16 22:37:46 -0800

    Username appears on user's profile after logging in

--- react-frontend/src/components/profile.js
+++ react-frontend/src/components/profile.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, {useState, Component} from "react";
 import "../css/profile.css"
 import icon from '../resources/StockFood.png';
 
@@ -24,18 +24,27 @@ let favorite_recipes = {
     "locmessage": "View"
 }
 
+
+
 const ProfilePage = (props) => {
+    const [username, setUsername] = useState(null);
     // Redirect if no credentials were placed in
     // If props has id, try to retrieve
     // If retrieval is not possible, return error
     console.log(props);
+
+    // may want to change get request from getting user to getting user's name only 
+    // b/c right now, we get all of user's info including password
+    UserDataService.getUser(localStorage.getItem("userID")).then( (user) => {
+        setUsername(user.data.username)
+    })
 
     return (
         <div className="container">
             <div className="row mt-3">
                 <div className="col-lg-3 card">
                     <img src={icon} alt="Profile Picture" className="card-img-top img-circle"/>
-                    <p className="text-center">{props.username ? props.username : "No Profile"}</p>
+                    <p className="text-center">{(username === null) ? "No Profile" : username}</p>
                     <button className="buttonAccount mb-2">Account Settings</button>
                     {/* Change username/password, Change profile pic, Change Visiblity, Delete Account */}
                     <button className="buttonAccount">Food Preferences</button>
