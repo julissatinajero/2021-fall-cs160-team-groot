acbd354535a23ef938ebcab3b31680d42d6601e1
Julissa Tinajero <julissatinajero99@gmail.com>
2021-11-17 00:25:58 -0800

    Reformats tests and adds a test to check for a missing username with included password

--- react-frontend/src/testing/cypress/integration/signInTests.js
+++ react-frontend/src/testing/cypress/integration/signInTests.js
@@ -19,7 +19,38 @@ describe('Sign In Tests', function () {
     cy.url().should('include', '/sign-in')
   })
 
-  // TEST 2: Unsuccessful sign in (Missing the password)
+  // TEST 2: Redirects if successfully signed in
+  it('Successful login', function () {
+    // Create a user 
+    cy.request({
+      method: 'POST',
+      url: 'http://localhost:8080/api/auth/signup',
+      body: {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'test@test.com',
+        username: 'test',
+        password: 'test1'
+      }
+    })
+
+    cy.visit('http://localhost:3000/sign-in')
+
+    // Submit the created user's username and password
+    cy.get('#username')
+      .should('be.visible')
+      .type('test')
+    cy.get('#password')
+      .should('be.visible')
+      .type('test1')
+    cy.get('button[type="submit"]')
+      .should('be.visible')
+      .click()
+
+    cy.url().should('include', '/profile')
+  })
+
+  // TEST 3: Unsuccessful sign in (Missing the password)
   it('Unsuccessful login: Submitting a form without a password', function () {
     cy.visit('http://localhost:3000/sign-in')
 
@@ -40,35 +71,25 @@ describe('Sign In Tests', function () {
     cy.url().should('include', '/sign-in')
   })
 
-  // TEST 3: Redirects if successfully signed in
-  it('Successful login', function () {
-    // Create a user 
-    cy.request({
-      method: 'POST',
-      url: 'http://localhost:8080/api/auth/signup',
-      body: {
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'test@test.com',
-        username: 'test',
-        password: 'test'
-      }
-    })
-
+  // TEST 4: Unsuccessful sign in (Missing the username)
+  it('Unsuccessful login: Submitting a form without a username', function () {
     cy.visit('http://localhost:3000/sign-in')
 
-    // Submit the created user's username and password
-    cy.get('#username')
-      .should('be.visible')
-      .type('test')
+    // Inputs a username; Doesn't input the password
     cy.get('#password')
       .should('be.visible')
-      .type('test')
+      .type('test1')
     cy.get('button[type="submit"]')
       .should('be.visible')
       .click()
+    // An window alert should pop up stating that their username or password is incorrect
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal(`Incorrect username or password!`)
+    })
+    // The validation checks should pop indicating a password is required
+    cy.get(':nth-child(1) > .error').should('be.visible')
 
-    cy.url().should('include', '/profile')
+    cy.url().should('include', '/sign-in')
   })
 
 })
