59d804596a77b3f969ea928308232448267521ad
Julissa Tinajero <julissatinajero99@gmail.com>
2021-11-16 22:35:08 -0800

    Adds a second test that checks for unsuccessful login

--- react-frontend/src/testing/cypress/integration/signInTest.js
+++ react-frontend/src/testing/cypress/integration/signInTest.js
@@ -1,5 +1,6 @@
 describe('Sign In Tests', function () {
 
+  // TEST 1: Redirects if successfully signed in
   it('Successfull login', function () {
       cy.request({
         method: 'POST', 
@@ -26,4 +27,21 @@ describe('Sign In Tests', function () {
   
       cy.url().should('include', '/profile')
   })
+
+  // TEST 2: Unsuccessful sign in
+  it('Unsuccessful login', function () {
+    cy.visit('http://localhost:3000/sign-in')
+
+    cy.get('#username')
+      .should('be.visible')
+      .type('test')
+    cy.get('#password')
+      .should('be.visible')
+      .type('randomIncorrectPassword')
+    cy.get('button[type="submit"]')
+      .should('be.visible')
+      .click()
+
+    cy.url().should('include', '/sign-in')
+})
 })
