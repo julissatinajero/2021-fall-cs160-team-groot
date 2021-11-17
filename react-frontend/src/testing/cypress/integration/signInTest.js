6d701744c727926c621b4b9f006b04b5eb88c921
Julissa Tinajero <julissatinajero99@gmail.com>
2021-11-16 22:16:35 -0800

    Test for successful signin

--- react-frontend/src/testing/cypress/integration/signInTest.js
+++ react-frontend/src/testing/cypress/integration/signInTest.js
@@ -1,10 +1,29 @@
-describe('Sign In Test', () => {
-  beforeEach(()=> {
-      cy.visit('http://localhost:3000/sign-in');
-  })
+describe('Sign In Tests', function () {
 
-  it('has a title', () => {
-      cy.contains('Sign In');
-  })
+  it('Successfull login', function () {
+      cy.request({
+        method: 'POST', 
+        url: 'http://localhost:8080/api/auth/signup', 
+        body: {
+          firstName: 'John',
+          lastName: 'Doe',
+          email: 'test@test.com',
+          username: 'test',
+          password: 'test'}
+      })
 
+      cy.visit('http://localhost:3000/sign-in')
+
+      cy.get('#username')
+        .should('be.visible')
+        .type('test')
+      cy.get('#password')
+        .should('be.visible')
+        .type('test')
+      cy.get('button[type="submit"]')
+        .should('be.visible')
+        .click()
+  
+      cy.url().should('include', '/profile')
+  })
 })
