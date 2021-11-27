db76332c7278a52e9a10fb9e473e2ec6f6d5b161
Nicolas Guerrero <Guerreronicolas1872@gmail.com>
2021-11-27 12:08:20 -0800

    Fix styling issue on the home page
    Fixes the excess padding issue for the image, now it takes up
    the right side of the screen. Also changed the page component
    from a class to a function, fitting in with the style of the
    recent changes.

--- react-frontend/src/components/homeMasthead.js
+++ react-frontend/src/components/homeMasthead.js
@@ -5,44 +5,43 @@ import head from '../resources/StockFood.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
-export default class HomeMasthead extends Component {
-    redirectSignin = () => {
-        this.props.history.push("/sign-in")
-    }
+const HomeMasthead = (props) => {
+    // Redirects; TODO: Turn the buttons into links to remove these functions
+    const redirectSignin = () => {
+        props.history.push("/sign-in")
+    };
 
-    redirectSignup = () =>{
-        this.props.history.push("/sign-up")
-    }
+    const redirectSignup = () =>{
+        props.history.push("/sign-up")
+    };
 
     // This will need to be changed when incorporating search terms
-    redirectSearch = () =>{
-        this.props.history.push("/search")
-    }
+    const redirectSearch = () =>{
+        props.history.push("/search")
+    };
 
-    render(){
-        // Before render
-        return(
-            <div className="container-fluid">
-                <div className="d-flex align-items-center row">
-                    <div className="col-7">
-                        <div className="mb-5">
-                            <h1 className="title ff-font d-flex justify-content-center">Food Finder</h1>
-                            <h2 className="d-flex justify-content-center">Start finding recipes now</h2>
-                            <div className="d-flex justify-content-center my-3">
-                                <input type="text" className="searchBar py-1"/>
-                                <button className="buttonSearch col-1 py-1" onClick={this.redirectSearch}><FontAwesomeIcon icon={faSearch}/></button>
-                            </div>                            
-                            <div className="row d-flex justify-content-center">
-                                <button className="buttonHome col-4 mx-2" onClick={this.redirectSignin}>Sign-In</button>
-                                <button className="buttonHome col-4 mx-2" onClick={this.redirectSignup}>Sign-Up</button>
-                            </div>
+    return(
+        <div className="container-fluid">
+            <div className="d-flex align-items-center padding-fix row">
+                <div className="col-7">
+                    <div className="mb-5">
+                        <h1 className="title ff-font d-flex justify-content-center">Food Finder</h1>
+                        <h2 className="d-flex justify-content-center">Start finding recipes now</h2>
+                        <div className="d-flex justify-content-center my-3">
+                            <input type="text" className="searchBar py-1"/>
+                            <button className="buttonSearch col-1 py-1" onClick={redirectSearch}><FontAwesomeIcon icon={faSearch}/></button>
+                        </div>                            
+                        <div className="row d-flex justify-content-center">
+                            <button className="buttonHome col-4 mx-2" onClick={redirectSignin}>Sign-In</button>
+                            <button className="buttonHome col-4 mx-2" onClick={redirectSignup}>Sign-Up</button>
                         </div>
                     </div>
-                    <div className="col-5 px-0">
-                        <img className="img-fluid" src={head}/>
-                    </div>
+                </div>
+                <div className="col-5 px-0">
+                    <img className="img-fluid" src={head}/>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
+export default HomeMasthead
